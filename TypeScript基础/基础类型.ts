b53a62645c3d8c58193cb14a6ce851a385ee498c
dengjian <jamis404@163.com>
2023-04-04 17:26:32 +0800

    笔记：ts的基础类型

--- TypeScript基础/基础类型.ts
+++ TypeScript基础/基础类型.ts
@@ -19,16 +19,33 @@ let u:undefined = undefined;
 //声明null
 let n:null = null;
 
+
 //空值（void）
 //javascript中没有空值的概念，在ts中可以表示没有任何返回值的函数
 function sayHi():void {
     console.log('hi');
 }
 
-//任意值（any）
+//任意值（any）顶级类型
 let anyThing:any = 10
+anyThing = sayHi
 anyThing = 'jam'
 anyThing = true
 anyThing = undefined
 anyThing = null
-anyThing = sayHi
+
+
+// 不知道的类型（unknown）顶级类型
+let unknownThing:unknown = 10;
+unknownThing = 'jam'
+unknownThing = true;
+
+/*
+* ts类型级别：
+* 1. top type顶级类型：any
+* 2. Object
+* 3. Number/String/Boolean
+* 4. number/string/boolean
+* 5. 字面量：1/'jam'/false
+* 6. never
+*/
